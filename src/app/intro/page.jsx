1cd0801b6fc6fa86dd8d14c917a0ba5d2629f023
ahammoud-05 <ayah.hammoud305@gmail.com>
2025-03-19 18:09:53 -0400

    navigation issues

--- src/app/intro/page.jsx
+++ src/app/intro/page.jsx
@@ -1,30 +1,163 @@
+"use client";
 import Image from "next/image";
 import Header from "../components/Header";
 import Link from "next/link";
-import CustomInput from "../components/CustomInput";
-
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const Intro = () => {
+
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+  const [step, setStep] = useState(1);
+  const router = useRouter();
+
+  const handleStepOne = async () => {
+    if (step === 1) {
+      if (typeof name === 'string' && name.trim() !== '' && isNaN(name)) {
+        console.log(name);
+        const res = await sendData(name)
+        if (res) {
+          setStep(2)
+          setName("")
+        } else {
+          alert("An error has occured.")
+        }
+      }
+    }
+  };
+
+  const handleStepTwo = async () => {
+    if (step === 2) {
+      if (typeof location === 'string' && location.trim() !== '' && isNaN(location)) {
+        console.log(location)
+        const res = await sendData(location)
+          if (res) {
+            router.push("/upload")
+          } else {
+            alert("An error has occured.")
+          }
+      }
+    }
+  }
+
+  const handleBackButton = () => {
+    if (step === 2) {
+      setStep(1);
+      setName("");
+    }
+  };
+
+  const sendData = async (name, location) => {
+    console.log({ name, location })
+    try {
+      const response = await fetch(
+        "https://us-central1-api-skinstric-ai.cloudfunctions.net/skinstricPhaseOne",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name: location }),
+        }
+      );
+
+      const result = await response.json();
+      return result;
+    } catch (error) {
+      console.error("Error sending data:", error);
+      return { success: false };
+    }
+  };
+
   return (
     <>
-    <div className="min-h-screen flex flex-col">
-    <Header />
-    <div className="ml-15 mt-5 font-semibold tracking-tight">
-        TO START ANALYSIS
-    </div>
-    <div className="flex-grow flex items-center justify-center w-full">
-        <Image src='/assets/rombuses.png' alt="rombus" width={762} height={762} />
-        <div className="absolute">
-            <CustomInput />
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <div className="ml-15 mt-5 font-semibold tracking-tight">
+          TO START ANALYSIS
         </div>
-    </div>
-        <Link className="flex items-center ml-10 mb-2 gap-5" href='/'>
-        <Image src='/assets/ButtonIcon.png' alt="back-button" width={44} height={44} />
-        <p className="whitespace-nowrap text-gray-700 font-semibold">BACK</p>
-        </Link>
-    </div>
+        <div className="flex-grow flex items-center justify-center w-full">
+          <Image
+            src="/assets/rombuses.png"
+            alt="rombus"
+            width={762}
+            height={762}
+          />
+          {/* INTRODUCE YOURSELF - INPUT */}
+          { step === 1 ? (
+            <div className="absolute">
+            <div className="flex flex-col items-center text-center">
+              <p className="text-xs uppercase tracking-wide text-gray-500 pb-3">
+                CLICK TO TYPE
+              </p>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Introduce Yourself"
+                className="text-4xl w-[395px] 
+                text-center outline-none border-b border-black max-w-md 
+                placeholder:text-black placeholder:font-light focus:placeholder-transparent
+                tracking-tighter"
+              />
+            </div>
+          </div>
+          ) : ''}
+
+          {/* WHERE ARE YOU FROM - INPUT */}
+          { step === 2 ? (
+            <div className="absolute">
+            <div className="flex flex-col items-center text-center">
+              <p className="text-xs uppercase tracking-wide text-gray-500 pb-3">
+                CLICK TO TYPE
+              </p>
+              <input
+                type="text"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                placeholder="Where are you from?"
+                className="text-4xl w-[395px] 
+                text-center outline-none border-b border-black max-w-md 
+                placeholder:text-black placeholder:font-light focus:placeholder-transparent
+                tracking-tighter"
+              />
+            </div>
+          </div>
+          ) : ''}
+        </div>
+
+        {/* BACK BUTTON */}
+        <div
+          onClick={handleBackButton}
+          className="flex items-center place-content-between ml-10 mr-10 mb-2 gap-5 cursor-pointer"
+        >
+          <Link
+            className="flex items-center gap-2"
+            href={step === 1 ? "/" : "/intro"}
+          >
+            <Image
+              src="/assets/ButtonIcon.png"
+              alt="back-button"
+              width={44}
+              height={44}
+            />
+            <p className="whitespace-nowrap text-gray-700 font-semibold">
+              BACK
+            </p>
+          </Link>
+
+          {/* PROCEED BUTTON */}
+          {(name && step === 1) || (location && step === 2) ? (  
+            <div onClick={step === 1 ? handleStepOne : handleStepTwo} className="flex items-center gap-2 cursor-pointer">
+              <p className="whitespace-nowrap text-gray-700 font-semibold">PROCEED</p>
+              <Image className="rotate-180" src="/assets/ButtonIcon.png" alt="back-button" width={44} height={44} />
+            </div>
+          ) : null}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Intro;
